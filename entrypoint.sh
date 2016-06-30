2209fae799f859b3823bea41e4b1c6ae2d2d96c4
Geo Van O <geo@eggheads.org>
2016-06-29 23:46:22 -0400

    Mount host directories for persistent data

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,7 @@
 
 cd /home/eggdrop/eggdrop
 if ! [ -z ${FIRSTRUN} ]; then USERFLAG="-m"; fi
+
 if [ -z ${SERVER} ]; then
   echo ""
   echo "--------------------------------------------------"
@@ -28,13 +29,32 @@ if [ -z ${SERVER} ]; then
   echo ""
   exit
 fi
+
 sed -i "/set nick \"Lamestbot\"/c\set nick \"$NICK\"" eggdrop.conf
 sed -i "/another.example.com:7000:password/d" eggdrop.conf
-sed -i "/you.need.to.change.this:6667/c\ \"${SERVER}\"" eggdrop.conf
+sed -i "/you.need.to.change.this:6667/c\ ${SERVER}" eggdrop.conf
 sed -i "/#listen 3333 all/c\listen ${LISTEN} all" eggdrop.conf
 sed -i "/#set owner \"MrLame, MrsLame\"/c\set owner \"${OWNER}\"" eggdrop.conf
-sed -i "/set userfile \"LamestBot.user\"/c\set userfile \"${USERFILE}\"" eggdrop.conf
-sed -i "/set chanfile \"LamestBot.chan\"/c\set chanfile \"${CHANFILE}\"" eggdrop.conf
+sed -i "/set userfile \"LamestBot.user\"/c\set userfile ${USERFILE}" eggdrop.conf
+sed -i "/set chanfile \"LamestBot.chan\"/c\set chanfile ${CHANFILE}" eggdrop.conf
 sed -i '/edit your config file completely like you were told/d' eggdrop.conf
 sed -i '/Please make sure you edit your config file completely/d' eggdrop.conf
+
+if mountpoint -q /home/eggdrop/eggdrop/host; then
+  if ! [ -a /home/eggdrop/eggdrop/host/eggdrop.conf ]; then
+    mv /home/eggdrop/eggdrop/eggdrop.conf /home/eggdrop/eggdrop/host/eggdrop.conf
+    ln -s /home/eggdrop/eggdrop/host/eggdrop.conf /home/eggdrop/eggdrop/eggdrop.conf
+  fi
+  if ! [ -a /home/eggdrop/eggdrop/host/eggdrop.user ]; then
+    mv /home/eggdrop/eggdrop/eggdrop.user /home/eggdrop/eggdrop/host/eggdrop.user
+    sed -i "/set chanfile ${USERFILE}/c\set chanfile host/${USERFILE}" eggdrop.conf
+    ln -s /home/eggdrop/eggdrop/host/eggdrop.user /home/eggdrop/eggdrop/eggdrop.user
+  fi
+  if ! [ -a /home/eggdrop/eggdrop/host/eggdrop.chan ]; then
+    mv /home/eggdrop/eggdrop/eggdrop.chan /home/eggdrop/eggdrop/host/eggdrop.chan
+    sed -i "/set chanfile ${CHANFILE}/c\set chanfile host/${CHANFILE}" eggdrop.conf
+    ln -s /home/eggdrop/eggdrop/host/eggdrop.chan /home/eggdrop/eggdrop/eggdrop.chan
+  fi
+fi
+
 ./eggdrop -n ${USERFLAG} ${CONFIG}
