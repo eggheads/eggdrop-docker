fac6f0403e72436185c85b6488b7784f90fdeafa
Geo Van O <geo@eggheads.org>
2016-07-05 22:35:41 -0400

    Don't require command line ENV if config already is mounted/exists

--- 1.6/entrypoint.sh
+++ 1.6/entrypoint.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 set -e
 
-if [ "$1" = 'eggdrop.conf' ]; then
-
 cd /home/eggdrop/eggdrop
-if ! [ -z ${FIRSTRUN} ]; then USERFLAG="-m"; fi
 
-if [ -z ${SERVER} ] || [ -z ${NICK}; then
+if ! [ -a /home/eggdrop/eggdrop/data/eggdrop.conf ] && ([ -z ${SERVER} ] || [ -z ${NICK} ]); then
   echo ""
   echo "--------------------------------------------------"
   echo "You have not set one of the required variables."
@@ -31,17 +28,17 @@ if [ -z ${SERVER} ] || [ -z ${NICK}; then
   echo "--------------------------------------------------"
   echo ""
   exit
+else
+  sed -i "/set nick \"Lamestbot\"/c\set nick \"$NICK\"" eggdrop.conf
+  sed -i "/another.example.com:7000:password/d" eggdrop.conf
+  sed -i "/you.need.to.change.this:6667/c\ ${SERVER}" eggdrop.conf
+  sed -i "/#listen 3333 all/c\listen ${LISTEN} all" eggdrop.conf
+  sed -i "/#set owner \"MrLame, MrsLame\"/c\set owner \"${OWNER}\"" eggdrop.conf
+  sed -i "/set userfile \"LamestBot.user\"/c\set userfile ${USERFILE}" eggdrop.conf
+  sed -i "/set chanfile \"LamestBot.chan\"/c\set chanfile ${CHANFILE}" eggdrop.conf
+  sed -i '/edit your config file completely like you were told/d' eggdrop.conf
+  sed -i '/Please make sure you edit your config file completely/d' eggdrop.conf
 fi
-
-sed -i "/set nick \"Lamestbot\"/c\set nick \"$NICK\"" eggdrop.conf
-sed -i "/another.example.com:7000:password/d" eggdrop.conf
-sed -i "/you.need.to.change.this:6667/c\ ${SERVER}" eggdrop.conf
-sed -i "/#listen 3333 all/c\listen ${LISTEN} all" eggdrop.conf
-sed -i "/#set owner \"MrLame, MrsLame\"/c\set owner \"${OWNER}\"" eggdrop.conf
-sed -i "/set userfile \"LamestBot.user\"/c\set userfile ${USERFILE}" eggdrop.conf
-sed -i "/set chanfile \"LamestBot.chan\"/c\set chanfile ${CHANFILE}" eggdrop.conf
-sed -i '/edit your config file completely like you were told/d' eggdrop.conf
-sed -i '/Please make sure you edit your config file completely/d' eggdrop.conf
 
 if ! mountpoint -q /home/eggdrop/eggdrop/data; then
   echo ""
