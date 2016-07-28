64fcd8353f5029c0840a6fd1dcdb4d06c35dc1d5
Joe Ferguson <joe@infosiftr.com>
2016-07-28 16:56:18 -0700

    Combine echoes to single cat with a heredoc

--- 1.6/entrypoint.sh
+++ 1.6/entrypoint.sh
@@ -4,30 +4,32 @@ set -e
 if [ "$1" = 'eggdrop.conf' ]; then
   cd /home/eggdrop/eggdrop
   if ! [ -e /home/eggdrop/eggdrop/data/eggdrop.conf ] && ([ -z ${SERVER} ] || [ -z ${NICK} ]); then
-    echo ""
-    echo "--------------------------------------------------"
-    echo "You have not set one of the required variables."
-    echo "The following variables must be set via the"
-    echo "-e command line argument in order to run eggdrop"
-    echo "for the first time:"
-    echo ""
-    echo "NICK   - set IRC nickname"
-    echo "SERVER - set IRC server to connect to"
-    echo ""
-    echo "Example:"
-    echo "docker run -ti -e NICK=DockerBot -e SERVER=irc.freenode.net eggdrop"
-    echo ""
-    echo "If you wish to telnet or DCC to your bot, you will"
-    echo "need to expose the docker port to your host by"
-    echo "adding -p 3333:3333 (or whatever port eggdrop is"
-    echo "listening on) to your docker run command."
-    echo ""
-    echo "These variables only need to be used the first"
-    echo "time you run the container- after the first use,"
-    echo "you can edit the config file created, directly."
-    echo "--------------------------------------------------"
-    echo ""
-    exit
+    cat <<EOS >&2
+
+--------------------------------------------------
+You have not set one of the required variables.
+The following variables must be set via the
+-e command line argument in order to run eggdrop
+for the first time:
+
+NICK   - set IRC nickname
+SERVER - set IRC server to connect to
+
+Example:
+docker run -ti -e NICK=DockerBot -e SERVER=irc.freenode.net eggdrop
+
+If you wish to telnet or DCC to your bot, you will
+need to expose the docker port to your host by
+adding -p 3333:3333 (or whatever port eggdrop is
+listening on) to your docker run command.
+
+These variables only need to be used the first
+time you run the container- after the first use,
+you can edit the config file created, directly.
+--------------------------------------------------
+
+EOS
+    exit 1
   else
     sed -i "/set nick \"Lamestbot\"/c\set nick \"$NICK\"" eggdrop.conf
     sed -i "/another.example.com:7000:password/d" eggdrop.conf
@@ -42,27 +44,30 @@ if [ "$1" = 'eggdrop.conf' ]; then
   fi
 
   if ! mountpoint -q /home/eggdrop/eggdrop/data; then
-    echo ""
-    echo "#####################################################"
-    echo "#####################################################"
-    echo "You did not specify a location on the host machine"
-    echo "to store your data. This means NOTHING will persist"
-    echo "if this docker container is deleted or updated, such"
-    echo "as user lists, chan lists, or ban lists."
-    echo ""
-    echo "In other words, you will likely LOSE YOUR DATA!"
-    echo ""
-    echo "Mounting a datastore on the host system will also"
-    echo "give you easy access to edit your configuration file."
-    echo ""
-    echo "If you wish to add the data store, simply run the"
-    echo "container again, but this time adding the option:"
-    echo "----------------------------------------------------"
-    echo "-v /path/to/your/saved/data/:/home/eggdrop/eggdrop/data"
-    echo "----------------------------------------------------"
-    echo "to your 'docker run' command."
-    echo "####################################################"
-    echo "####################################################"
+    cat <<EOS
+
+#####################################################
+#####################################################
+You did not specify a location on the host machine
+to store your data. This means NOTHING will persist
+if this docker container is deleted or updated, such
+as user lists, chan lists, or ban lists.
+
+In other words, you will likely LOSE YOUR DATA!
+
+Mounting a datastore on the host system will also
+give you easy access to edit your configuration file.
+
+If you wish to add the data store, simply run the
+container again, but this time adding the option:
+----------------------------------------------------
+-v /path/to/your/saved/data/:/home/eggdrop/eggdrop/data
+----------------------------------------------------
+to your 'docker run' command.
+####################################################
+####################################################
+
+EOS
   fi
 
   mkdir -p /home/eggdrop/eggdrop/data
